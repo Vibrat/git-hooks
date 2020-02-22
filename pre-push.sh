67845e27a515242f533ff5534ad7500b0d306088
Lam Nguyen <lam.nguyen@oursky-sea.com>
2020-02-22 16:26:56 +0700

    add pre-push sample

---
+++ pre-push.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+read -p "Are you ready to push to remote repo? [Y|n]" -n 1 -r < /dev/tty
+
+# new line
+echo
+if echo $READ | grep '^[Nn]$' > /dev/null
+then
+  echo "abortion, push failed"
+  exit 1
+fi
+
+## code linter here
+
+if [[ $? != 0 ]]
+then
+  echo "Linter failed"
+  exit 1
+else
+  echo "ready to push"
+  exit 0
+fi
